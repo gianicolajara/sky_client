f28fe071d0c9057944e714e4e21d6169a3bab3e8
gianicola <gianicolajara@gmail.com>
2024-12-05 02:16:16 -0400

    avoid / path

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  router.replace("/home");
+
   return <div className="text-white">Nothing to see over here</div>;
 }
