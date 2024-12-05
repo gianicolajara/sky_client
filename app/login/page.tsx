2241199e90d6da551f650840f2f72ab6da4220f3
gianicola <gianicolajara@gmail.com>
2024-12-05 14:08:41 -0400

    adding padding login and delete id from use posts

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,14 +4,12 @@ import BigLogo from "@/components/shared/Logo/BigLogo";
 function page() {
   return (
     <section className="w-full h-screen justify-center items-center grid grid-cols-1 grid-rows-1 lg:grid-cols-2 lg:grid-rows-1">
-      <div className="h-full max-w-[1200px] mx-auto px-2 py-4">
-        <div className="w-full h-full lg:flex flex-col justify-center items-center bg-cyan-400 hidden">
-          <BigLogo className="lg:flex hidden" />
-        </div>
-        <div className="w-full h-full flex justify-center items-center bg-cyan-400 lg:bg-transparent flex-col gap-y-4">
-          <BigLogo />
-          <LoginCard />
-        </div>
+      <div className="w-full h-full lg:flex flex-col justify-center items-center bg-cyan-400 hidden">
+        <BigLogo className="lg:flex hidden" />
+      </div>
+      <div className="w-full h-full flex justify-center items-center bg-cyan-400 lg:bg-transparent flex-col gap-y-4 px-2">
+        <BigLogo />
+        <LoginCard />
       </div>
     </section>
   );
--- hooks/usePosts/index.ts
+++ hooks/usePosts/index.ts
@@ -1,9 +1,9 @@
 import { getPostByFollowingService } from "@/services/post";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
-export const usePosts = (limit: number, id?: string) => {
+export const usePosts = (limit: number) => {
   const getPostsByFollowing = useInfiniteQuery({
-    queryKey: ["getPostsByFollowing", limit, id],
+    queryKey: ["getPostsByFollowing", limit],
     queryFn: ({ pageParam }) => getPostByFollowingService(limit, pageParam),
     initialPageParam: 1,
     getNextPageParam: (lastPage) => lastPage.nextPage,
