f3d61cafb50eca3c98cf5c6a6e63d1599ea05057
gianicola <gianicolajara@gmail.com>
2024-12-04 23:03:43 -0400

    u

--- services/csrfToken.ts
+++ services/csrfToken.ts
@@ -1,7 +1,7 @@
 import { axiosWithCredentialsInstance } from "@/lib/fetchUtility";
 
 export const getCsrfTokenService = async () => {
-  const res = await axiosWithCredentialsInstance().get("auth/csrfToken");
+  const res = await axiosWithCredentialsInstance().get("/auth/csrfToken");
   if (res.status === 200) return res.data.csrfToken;
   else throw new Error(res.data.message);
 };
